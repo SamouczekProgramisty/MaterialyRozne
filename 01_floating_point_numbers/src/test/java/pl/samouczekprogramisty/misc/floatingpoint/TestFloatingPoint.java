011d8c0abf1a59fa7b77f46161b620c5cc48dc66
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-09-28 22:32:17 +0200

    Fixing bug in floating number converter

--- 01_floating_point_numbers/src/test/java/pl/samouczekprogramisty/misc/floatingpoint/TestFloatingPoint.java
+++ 01_floating_point_numbers/src/test/java/pl/samouczekprogramisty/misc/floatingpoint/TestFloatingPoint.java
@@ -5,9 +5,7 @@ import pl.samouczekprogramisty.misc.floatingpoint.FloatingPoint.Format;
 
 import java.math.BigDecimal;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class TestFloatingPoint {
@@ -19,12 +17,17 @@ public class TestFloatingPoint {
 
     @Test
     public void shouldBeAbleToParseSinglePrecisionFloatingPointNumber2() {
-        assertThat(FloatingPoint.instantiate(Format.SINGLE_PRECISION, "270.125").toString(), is(equalTo("0 01111011 10011001100110011001101")));
+        assertThat(FloatingPoint.instantiate(Format.SINGLE_PRECISION, "270.125").toString(), is(equalTo("0 10000111 00001110001000000000000")));
     }
 
     @Test
     public void shouldBeAbleToParseDoublePrecisionFloatingPointNumber() {
         assertThat(FloatingPoint.instantiate(Format.DOUBLE_PRECISION, "0.1").toString(), is(equalTo("0 01111111011 1001100110011001100110011001100110011001100110011010")));
+    }
+
+    @Test
+    public void shouldBeAbleToParseDoublePrecisionFloatingPointNumber2() {
+        assertThat(FloatingPoint.instantiate(Format.DOUBLE_PRECISION, "270.125").toString(), is(equalTo("0 10000000111 0000111000100000000000000000000000000000000000000000")));
     }
 
     @Test
